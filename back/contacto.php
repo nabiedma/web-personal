5fd6822ecfb9be3639f6e1c3ba12a159be5c989e
nabiedma <nabiedma@gmail.com>
2020-02-01 16:08:28 -0300

    Se agrego contacto.php - modificacion mailer

--- back/contacto.php
+++ back/contacto.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
 
 echo '<pre>';
 var_dump($_POST);
@@ -21,4 +27,32 @@ if (isset($_POST['submit'])) {
         $response = file_get_contents($url);
         $response = json_decode($response);
     }
+}
+
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 587; // TLS only
+$mail->SMTPSecure = 'tls'; // ssl is deprecated
+$mail->SMTPAuth = true;
+$mail->Username = 'youremail@gmail.com'; // email
+$mail->Password = 'PASSWORD'; // password
+$mail->setFrom('system@cksoftwares.com', 'AB Contacto'); // From email and name
+$mail->addAddress('nabiedma@gmail.com', 'Alejandro Biedma'); // to email and name
+$mail->Subject = 'Mensaje del Formulario de Contacto';
+$mail->msgHTML("test body"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'HTML messaging not supported'; // If html emails is not supported by the receiver, show this body
+// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+$mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+if(!$mail->send()){
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}else{
+    echo "Message sent!";
 }
